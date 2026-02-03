ea8f9c583452f0f371c709474edba14c8110ae6b
Artem <wannacry1664@gmail.com>
2026-02-03 17:35:43 +0300

    fix: allow frontend origins via CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,10 +1,29 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://badson-cms-production.up.railway.app'],
+      origin: [
+        'https://wvnnvcry.github.io',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
     },
   },
   'strapi::poweredBy',
